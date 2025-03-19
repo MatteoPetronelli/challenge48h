94840bfc332b1d58c9d44e3f19a04eaf03e1a39d
MatteoPetronelli <matteo.petronelli5@gmail.com>
2025-03-19 18:53:46 +0100

    add logout button

--- frontend/assets/js/loggedIn.js
+++ frontend/assets/js/loggedIn.js
@@ -1,9 +1,21 @@
 window.addEventListener('DOMContentLoaded', () => {
     const userData = localStorage.getItem('user');
     const loggedInDiv = document.querySelector('.loggedIn');
+    const logoutBtn = document.getElementById('logoutBtn');
 
     if (userData && loggedInDiv) {
         const user = JSON.parse(userData);
         loggedInDiv.textContent = `Bienvenue, ${user.registerPrenom} ${user.registerName} !`;
+
+        logoutBtn.style.display = 'inline-block';
+
+        logoutBtn.addEventListener('click', () => {
+            localStorage.removeItem('user');
+            loggedInDiv.textContent = 'Vous êtes déconnecté.';
+            logoutBtn.style.display = 'none';
+
+            // Optionnel : redirige vers login/register
+            window.location.href = 'login_register.html';
+        });
     }
 });
